46b7956d79a5c6ffdcd9ce0ae65a326eccf8f8d1
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2021-08-18 13:33:08 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Date
+
+const today = new Date();
+
+const date = today.getDate(); // the Date
+
+const day = today.getDay(); // the day of the week
+
+const year = today.getFullYear();
+
+console.log(year);
